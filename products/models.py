0d393740db8776672719d0d52ec09f7d6fcdf955
reddycloudengineer <reddy.cloudengineer@gmail.com>
2019-03-26 07:19:35 -0400

    Created Products Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    """
+    Products Model
+    Defines the attributes of a product
+    """
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_price(self):
+        return self.price
+
+    def __repr__(self):
+        return self.name + ' is added.'
